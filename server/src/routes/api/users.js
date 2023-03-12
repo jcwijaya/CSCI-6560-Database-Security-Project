e05f01f16e399132fc1125c3eb2a2993ec318f10
DuBrowski <krdubray@comcast.net>
2023-03-12 16:06:35 -0500

    add auth to some users routes

--- server/src/routes/api/users.js
+++ server/src/routes/api/users.js
@@ -3,6 +3,7 @@ import { v1 as uuidv1 } from "uuid";
 import bcrypt from "bcrypt";
 const saltRounds = 10;
 import connection from "../../services/database.js";
+import auth from "../../middlewares/auth.js";
 
 const usersRouter = express.Router();
 
@@ -31,7 +32,9 @@ usersRouter.post("/", async function (req, res) {
 });
 
 //Get a user's information using the id that was returned from the POST /user
-usersRouter.get("/:user_id", async function (req, res) {
+usersRouter.get("/:user_id", auth, async function (req, res) {
+  if (req.user.user_id !== req.params.user_id)
+    return res.status(401).json({ errors: [{ msg: "Action unauthorized" }] });
   await connection.query(
     "SELECT * FROM `user` WHERE user_id = ?",
     req.params.user_id,
@@ -49,35 +52,38 @@ usersRouter.get("/:user_id", async function (req, res) {
   );
 });
 
- //Get all users
-usersRouter.get("/", function(req, res) {
-   connection.query(
-     "SELECT * FROM `user`",
-     function(error, results) {
-       if(error){
-         console.log(error);
-         res.status(500).send({message:"There was an error connecting to the database: ", error });
-       }
-       console.log("Database query successful: ", results)
-       res.status(200).json(results);
-     }
-   );
- });
+//Get all users
+usersRouter.get("/", function (req, res) {
+  connection.query("SELECT * FROM `user`", function (error, results) {
+    if (error) {
+      console.log(error);
+      res.status(500).send({
+        message: "There was an error connecting to the database: ",
+        error,
+      });
+    }
+    console.log("Database query successful: ", results);
+    res.status(200).json(results);
+  });
+});
 
- //Delete user
-usersRouter.delete("/", async function(req, res) {
-
-  const {user_id, password} = req.body;
-  const encryptedPassword = await bcrypt.hash(password, saltRounds);
+//Delete user
+usersRouter.delete("/:user_id", auth, async function (req, res) {
+  if (req.user.user_id !== req.params.user_id)
+    return res.status(401).json({ errors: [{ msg: "Action unauthorized" }] });
   connection.query(
-    "DELETE FROM `user` WHERE user_id = ?", [user_id],
-    function(error, results) {
-      if(error){
+    "DELETE FROM `user` WHERE user_id = ?",
+    [req.user.user_id],
+    function (error, results) {
+      if (error) {
         console.log(error);
-        res.status(500).send({message:"There was an error connecting to the database: ", error });
+        res.status(500).send({
+          message: "There was an error connecting to the database: ",
+          error,
+        });
       }
-      console.log("User deleted successfully: ", results)
-      res.status(200).json({results, message: "User deleted successfully"});
+      console.log("User deleted successfully: ", results);
+      res.status(200).json({ results, message: "User deleted successfully" });
     }
   );
 });
