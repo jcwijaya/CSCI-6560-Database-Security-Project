34e5d8f8e7255b8273f7367d2405a795fe98a19d
DuBrowski <krdubray@comcast.net>
2023-04-02 00:23:44 -0500

    file downloads endpoint

---
+++ server/src/routes/api/downloads.js
@@ -0,0 +1,64 @@
+import express from "express";
+import connection from "../../services/database.js";
+import storage from "../../configs/storage.config.js";
+import auth from "../../middlewares/auth.js";
+import roleAuth from "../../middlewares/roleAuth.js";
+
+const downloadsRouter = express.Router();
+
+// @route    GET api/downloads
+// @desc     download file from bucket ("read")
+// @access   PRIVATE
+/* body parameters: 
+{
+  file_id
+  bucket_id
+  version?: unique version identifier for a file. Only needed if 
+    user is trying to download inactive version of a file.
+}
+*/
+
+//TODO: Route through roleAuth middleware once uploads implemented-
+//  potentially will have separate role authorization middleware
+//  whenever target user not involved, like in this case.
+downloadsRouter.get("/", auth, async (req, res) => {
+  // get necessary body parameters- only need version if user is trying
+  // to download inactive version of a file
+  let version = req.body?.version;
+  const { file_id, bucket_id } = req.body;
+
+  // Query database to check for validity
+  let query, valuesArr;
+  if (version) {
+    query =
+      "SELECT * FROM file WHERE file_id= ? AND bucket_id = ? AND version = ?";
+    valuesArr = [file_id, bucket_id, version];
+  } else {
+    query =
+      "SELECT * FROM file WHERE file_id=? AND bucket_id = ? and isActive = 1";
+    valuesArr = [file_id, bucket_id];
+  }
+  connection.query(query, valuesArr, (error, results) => {
+    if (error) throw error;
+
+    if (results.length < 1 || results.length > 2)
+      return res.status(400).json({
+        errors: [
+          {
+            msg: "Bad request- invalid number of results received.",
+          },
+        ],
+      });
+
+    console.log("Download possible. Results:");
+    console.log(results);
+
+    return res.status(201).json({
+      results,
+    });
+  });
+
+  // TODO: Call downloader service to download file from bucket
+});
+
+export default downloadsRouter;
---
+++ server/src/services/downloader.js
@@ -0,0 +1,28 @@
+import storage from "../configs/storage.config";
+
+// download service for google cloud storage
+// generation number can be provided to download inactive version of file
+const downloader = async (
+  bucketName,
+  fileName,
+  destFileName,
+  generation = null
+) => {
+  // all params required
+  if (!bucketName || !fileName || !destFileName) {
+    throw new Error({
+      msg: "please provide bucket name, file name, and destination",
+    });
+  }
+
+  const options = {
+    destination: destFileName,
+  };
+
+  // download file
+  await storage.bucket(bucketName).file(fileName).download(options);
+
+  console.log(`gs://${bucketName}/${fileName} downloaded to ${destFileName}.`);
+};
+
+export default downloader;
